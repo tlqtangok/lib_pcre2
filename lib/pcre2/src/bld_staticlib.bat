327df4a1f7ad6fd5ec6c5cfc158ad774dde64512
林奇 <tlqtangok@126.com>
2025-10-21 16:47:04 +0800

    bld can link lib

--- lib/pcre2/src/bld_staticlib.bat
+++ lib/pcre2/src/bld_staticlib.bat
@@ -1,30 +1,36 @@
+set VS_CL_OPT_NODEBUG=/c /ZI /nologo /W3 /WX- /diagnostics:column /sdl /O2 /Oi /D NDEBUG /D _CONSOLE /D _UNICODE /D UNICODE /Gm- /EHsc /MDd /GS /Gy /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /permissive-  /external:W3 /Gd /TP /FC /errorReport:prompt
+
+set VS_CL_OPT_DEBUG=/c /ZI /JMC /nologo /W3 /WX- /diagnostics:column /sdl /O2 /D DEBUG /D _CONSOLE /D _UNICODE /D UNICODE /Gm- /EHsc /MDd /GS /fp:precise /Zc:wchar_t /Zc:forScope /Zc:inline /permissive-  /external:W3 /Gd /TP /FC /errorReport:prompt
+
+
+set VS_LIB_OPT=/NOLOGO /MACHINE:X64
+
+set VS_LINK_OPT=/ERRORREPORT:PROMPT  /INCREMENTAL /MANIFEST  /manifest:embed /DEBUG /SUBSYSTEM:CONSOLE /TLBID:1 /DYNAMICBASE /NXCOMPAT /MACHINE:X64 /MANIFESTUAC:"level='asInvoker' uiAccess='false'"
+
+
+set INC=..\..\pcre2\include
+
+
 @echo off
-:: Build static library
-cl /c /W4 /I"..\..\pcre2\include" /DPCRE2_STATIC /EHsc regex.cpp
-lib /OUT:regex_8.lib regex.obj
+where cl.exe 
+if not %errorlevel% equ 0 (
+    echo "- please setup your vs2022 environment"
+    exit /b -1
+)
+@echo on
+
+
+
+:: important : no /D DEF_DLL_EXPORTS in VS_CL_OPT !!!
+CL.exe  %VS_CL_OPT_NODEBUG% /I"%INC%" *.cpp
+lib.exe %VS_LIB_OPT% /OUT:"regex_8.lib"  *.obj ..\..\pcre2\lib\pcre2-8.lib
 
 copy /Y regex_8.lib ..\..\pcre2\lib\
 copy /Y regex.h ..\..\pcre2\include\
 
-del regex_8.lib *.obj
+del regex_8.lib *.obj *.pdb *.idb
 
 
-
-
-if 0 == 1 (
-# Build DLL
-cl /LD /W4 /I"d:\jd\t\git\lib_pcre2\lib\pcre2\include" /DPCRE2_STATIC /DREGEX_EXPORTS /EHsc regex.cpp /link /LIBPATH:"d:\jd\t\git\lib_pcre2\lib\pcre2\lib" pcre2-8.lib /OUT:regex.dll
-
-# Use static library
-cl /W4 /I"d:\jd\t\git\lib_pcre2\lib\pcre2\include" /DPCRE2_STATIC /EHsc main.cpp regex.lib /link /LIBPATH:"d:\jd\t\git\lib_pcre2\lib\pcre2\lib" pcre2-8.lib
-
-
-# Step 1: Rebuild regex.lib without PCRE2_STATIC
-cl /c /W4 /I"d:\jd\t\git\lib_pcre2\lib\pcre2\include" /EHsc regex.cpp
-lib /OUT:regex.lib regex.obj
-
-# Step 2: Link with dynamic PCRE2
-cl /W4 /I"d:\jd\t\git\lib_pcre2\lib\pcre2\include" /EHsc main.cpp regex.lib /link /LIBPATH:"d:\jd\t\git\lib_pcre2\lib\pcre2\lib" pcre2-8.lib
-)
-
-@echo on 
+::CL.exe  %VS_CL_OPT_DEBUG% /I"%INC%" ..\src\test.cpp
+::link.exe %VS_LINK_OPT% /implib:"slib.lib" /out:"mainapp.exe"  test.obj slib.lib 
+::.\mainapp.exe
--- lib/readme.txt
+++ lib/readme.txt
@@ -1,1 +1,9 @@
 win11 + vs2022 
+
+set vsproj as following:
+
+======
+      <AdditionalIncludeDirectories> $(SolutionDir)_3rd\dep;$(SolutionDir)_3rd\fmt\include;$(SolutionDir)_3rd\opencv3.4.16\build\include;$(SolutionDir)_3rd\pcre2\include </AdditionalIncludeDirectories>
+      <AdditionalLibraryDirectories> $(SolutionDir)_3rd\fmt\lib;$(SolutionDir)_3rd\opencv3.4.16\build\x64\vc15\lib;$(SolutionDir)_3rd\pcre2\lib </AdditionalLibraryDirectories>
+      <AdditionalDependencies> fmt_slib_10.1.0.lib;opencv_world3416d.lib;regex_8.lib;$(CoreLibraryDependencies);%(AdditionalDependencies) </AdditionalDependencies>
+======
